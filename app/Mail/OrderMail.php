d1ba03835cad071136c635696ca4534a20470f04
Premchandra Yadav <97802015+prem2u@users.noreply.github.com>
2023-08-30 11:51:11 +0530

    Add files via upload

---
+++ app/Mail/OrderMail.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace App\Mail;
+
+use App\Models\Order;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Mail\Mailable;
+use Illuminate\Mail\Mailables\Content;
+use Illuminate\Mail\Mailables\Envelope;
+use Illuminate\Queue\SerializesModels;
+
+class OrderMail extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    public Order $order;
+
+    /**
+     * Create a new message instance.
+     *
+     * @return void
+     */
+    public function __construct($order)
+    {
+        $this->order = $order;
+    }
+
+    /**
+     * Get the message envelope.
+     *
+     * @return \Illuminate\Mail\Mailables\Envelope
+     */
+    public function envelope()
+    {
+        return new Envelope(
+            subject: 'Order Mail',
+        );
+    }
+
+    /**
+     * Get the message content definition.
+     *
+     * @return \Illuminate\Mail\Mailables\Content
+     */
+    // public function content()
+    // {
+    //     return new Content(
+    //         view: 'view.name',
+    //     );
+    // }
+     /**
+     * Get the attachments for the message.
+     *
+     * @return $this
+     */
+    public function build()
+    {
+        return $this->subject('Order Confirmation')->view('mails.order-mail');
+    }
+
+    /**
+     * Get the attachments for the message.
+     *
+     * @return array
+     */
+    public function attachments()
+    {
+        return [];
+    }
+}
